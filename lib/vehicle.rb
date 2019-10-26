9e10aa0b2de5b6452d3b9eeff6494fdf3499c9df
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 03:48:46 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,13 +4,13 @@ class Vehicle
   def initialize(wheel_size,wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
+  end
+  def go 
+    "vrrrrrrrooom!"
+  end 
     
-    def go 
-      "vrrrrrrrooom!"
-    end 
-    
-    def fill_up_tank
-      "filling up!"
-    end
+  def fill_up_tank
+    "filling up!"
+  end
     
 end
